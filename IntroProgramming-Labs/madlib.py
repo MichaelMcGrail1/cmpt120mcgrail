e46a898c75e6c01701fb103ab3c565903807305f
MichaelMcGrail1 <35709447+MichaelMcGrail1@users.noreply.github.com>
2018-02-02 21:11:02 -0600

    Solution to Mad Libs exercise.

---
+++ IntroProgramming-Labs/madlib.py
@@ -0,0 +1,9 @@
+# Michael McGrail
+# Introduction to Programming
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+print("Your MadLib:")
+print("The", adjective, noun, "from", place, verb, "across the shaky bridge.")
