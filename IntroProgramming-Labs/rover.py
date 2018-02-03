a61e574a1e36a6eba1fc12bc6a1d8ed31298e96a
MichaelMcGrail1 <35709447+MichaelMcGrail1@users.noreply.github.com>
2018-02-02 18:59:04 -0600

    Solution to Curiosity rover exercise.

---
+++ IntroProgramming-Labs/rover.py
@@ -0,0 +1,12 @@
+# Michael McGrail
+# Introduction to Programming
+# A program calculating the time it takes to send pictures from Mars to NASA
+
+def main():
+    distance = (34000000)
+    speed = (186000)
+    time = (distance / speed)
+    print(time)
+                   
+main()
+    
